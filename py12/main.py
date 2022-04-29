4a1812f85777a69c79aff4780947a72f670a0323
958649315@qq.com <958649315@qq.com>
2022-04-29 15:49:03 +0800

    init

--- py12/main.py
+++ py12/main.py
@@ -2,15 +2,7 @@ import os
 import sys
 from openpyxl import Workbook
 from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
 import time
-from datetime import datetime
-############################
-# github.com/foreverlz1111 #
-############################
-__author__ = "foreverlz1111"
-__license__ = "GPL3.0"
-
 
 ########
 # 变量 #
@@ -18,15 +10,15 @@ __license__ = "GPL3.0"
 files_name = {"answer": "answer.xlsx", "students": "students.xlsx"}
 dir_name = {"students_score_out": "out", "students_answer": "student"}
 
-
 ########
 # 常量 #
 ########
 
-__uname = os.uname()
+
 __current_cwd = os.getcwd()
 
-__denote = "*启动目录自动新建"+dir_name["students_score_out"]+"文件夹存放学生成绩.xlsx文件、"+ dir_name["students_answer"]+"文件夹存放学生答案.xlsx文件\n*默认在启动文件夹内读取answer.xlsx文件作为答案,读取students.xlsx文件作为花名册\n"
+__denote = "*启动目录自动新建" + dir_name["students_score_out"] + "文件夹存放学生成绩.xlsx文件、" + dir_name[
+    "students_answer"] + "文件夹存放学生答案.xlsx文件\n*默认在启动文件夹内读取answer.xlsx文件作为答案,读取students.xlsx文件作为花名册\n"
 __borderline = "************************************************************"
 dic_name = {
     "students_score_out": "成绩输出目录",
@@ -35,54 +27,57 @@ dic_name = {
     "add_student": "\n将文件放入指定目录后(文件夹不会被检测到)，按下[回车键]以继续",
     "answer_notfound": "没找到答案文件，请确保其在启动目录并命名为" + files_name["answer"],
     "students_notfound": "没找到花名册文件，请确保其在启动目录并命名为" + files_name["students"],
-    "answer_found":__current_cwd + " 目录已找到文件：" + files_name["answer"],
-    "students_found":__current_cwd + " 目录已找到文件：" + files_name["students"],
-    "choice_notfound":"没找到对应命令，请重新输入。",
-    "processing":"处理中...",
-    "score_title":["学号","姓名","班级","成绩","题目总分"],
-    "missing_title":["学号","姓名","班级","未提交"],
-    "yes":"是",
-    "save_error":"处理失败！",
-    "save_success":"处理成功，查看文件 ",
-    "saved_path": __current_cwd + "/" +dir_name["students_score_out"]+"/"
+    "answer_found": __current_cwd + " 目录已找到文件：" + files_name["answer"],
+    "students_found": __current_cwd + " 目录已找到文件：" + files_name["students"],
+    "choice_notfound": "没找到对应命令，请重新输入。",
+    "processing": "处理中...",
+    "score_title": ["学号", "姓名", "班级", "成绩", "题目总分"],
+    "missing_title": ["学号", "姓名", "班级", "未提交"],
+    "yes": "是",
+    "save_error": "处理失败！",
+    "save_success": "处理成功，查看文件 ",
+    "saved_path": __current_cwd + "/" + dir_name["students_score_out"] + "/"
 }
 __menu_name = {
-    "menu":"\n选择菜单：\n",
-    "choice1":"输入1：重新查找文件",
-    "choice2":"输入2：修改学生的作答",
-    "choice3":"输入3：统计未提交作业的学生名单 ",
-    "choice_exit":"输入q：退出本程序",
-    
+    "menu": "\n选择菜单：\n",
+    "choice1": "输入1：重新查找文件",
+    "choice2": "输入2：批改学生的成绩",
+    "choice3": "输入3：统计未提交作业的学生名单 ",
+    "choice_exit": "输入q：退出本程序",
+
 }
+
 
 def print_menu():
     for x in __menu_name:
         print(__menu_name[x])
     print(__borderline)
-    
+
+
 def catch_choice(c):
     # 版本兼容性，暂不升级为match
-    if(c == "1"):
+    if (c == "1"):
         check_files()
         print_menu()
         return True
-    elif(c == "2"):
+    elif (c == "2"):
         output_score()
         print_menu()
         return True
-    elif(c == "3"):
+    elif (c == "3"):
         output_missing()
         print_menu()
         return True
-    elif(c == "4"):
+    elif (c == "4"):
         return True
-    elif(c == "q"):
+    elif (c == "q"):
         return False
     else:
         print(dic_name["choice_notfound"])
         print_menu()
         return True
-    
+
+
 def menu():
     print_menu()
     userexit = True
@@ -95,11 +90,12 @@ def menu():
             print(dic_name["keyboard_exit"])
             exit()
 
+
 def output_missing():
     # only check file if missing
     print(dic_name["processing"])
-    students = load_workbook(filename = files_name["students"])
-    students_sheet1 = students["工作表1"] # 输出的成绩花名册
+    students = load_workbook(filename=files_name["students"])
+    students_sheet1 = students["Sheet"]  # 输出的成绩花名册
     students_table = get_table(students_sheet1)
     wb = Workbook()
     ws = wb.active
@@ -113,19 +109,20 @@ def output_missing():
         except:
             continue
     try:
-        wb.save(dir_name["students_score_out"]+"/"+output_xlsx_name("未提交"))
-        print(dic_name["save_success"],dic_name["saved_path"]+output_xlsx_name("未提交"))
+        wb.save(dir_name["students_score_out"] + "/" + output_xlsx_name("未提交"))
+        print(dic_name["save_success"], dic_name["saved_path"] + output_xlsx_name("未提交"))
     except OSError:
         print(dic_name["save_error"])
     print(__borderline)
+
 
 def output_score():
     # check file for each
     print(dic_name["processing"])
     answer_sheet1 = load_and_sheet1(files_name["answer"])
     answer_table = get_table(answer_sheet1)
-    students = load_workbook(filename = files_name["students"])
-    students_sheet1 = students["工作表1"] # 输出的成绩花名册
+    students = load_workbook(filename=files_name["students"])
+    students_sheet1 = students["Sheet"]  # 输出的成绩花名册
     students_table = get_table(students_sheet1)
     wb = Workbook()
     ws = wb.active
@@ -133,7 +130,7 @@ def output_score():
     ws.append(dic_name["score_title"])
     for x in students_table:
         try:
-            single = load_workbook(filename = students_answer_filename(x[0]))
+            single = load_workbook(filename=students_answer_filename(x[0]))
             single_sheet1 = single["Sheet"]
             single_table = get_table(single_sheet1)
         except FileNotFoundError:
@@ -141,7 +138,7 @@ def output_score():
         score = 0
         for a in single_table:
             for b in answer_table:
-                if a[0]==b[0]:
+                if a[0] == b[0]:
                     if a[1] == b[1]:
                         score += b[2]
                 else:
@@ -150,37 +147,41 @@ def output_score():
         x.append(score_total)
         ws.append(x)
     try:
-        wb.save(dir_name["students_score_out"]+"/"+output_xlsx_name("成绩"))
-        print(dic_name["save_success"],dic_name["saved_path"]+output_xlsx_name("成绩"))
+        wb.save(dir_name["students_score_out"] + "/" + output_xlsx_name("成绩"))
+        print(dic_name["save_success"], dic_name["saved_path"] + output_xlsx_name("成绩"))
     except OSError:
         print(dic_name["save_error"])
     print(__borderline)
 
-    
+
 def students_answer_filename(x):
-    return dir_name["students_answer"] +"/"+ str(x) + ".xlsx"
+    return dir_name["students_answer"] + "/" + str(x) + ".xlsx"
+
 
 def output_xlsx_name(s):
     mytime = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    filename = s+str(mytime)+".xlsx"
+    filename = s + str(mytime) + ".xlsx"
     return filename
+
 
 def get_total(t):
     score = 0
     for c in t:
         score += c[2]
     return score
-    
+
+
 def load_and_sheet1(f):
-    wb = load_workbook(filename = f)
-    sheet1 = wb["工作表1"]
+    wb = load_workbook(filename=f)
+    sheet1 = wb["Sheet"]
     return sheet1
-    
+
+
 def get_table(sheet):
     cells = []
     table = []
     p = False
-    for x in sheet.iter_rows(min_row = 2):
+    for x in sheet.iter_rows(min_row=2):
         cells = []
         for cell in x:
             if cell.value is None:
@@ -189,9 +190,11 @@ def get_table(sheet):
         if cells != []:
             table.append(cells)
     return table
-            
+
+
 def get_sysinfo():
     if sys.platform.startswith("linux"):
+        __uname = os.uname()
         print(__uname.sysname, __uname.release, __uname.machine)
         # time.sleep(1)
         # os.chdir("/home")
@@ -206,7 +209,7 @@ def get_varinfo():
 
 
 def create_dir():
-    dir_error =  "位置异常"
+    dir_error = "位置异常"
     dir_exist = "已存在，跳过新建"
     dir_mk = "已自动新建文件夹"
     for x in dir_name:
@@ -215,11 +218,11 @@ def create_dir():
             if not os.path.exists(p):
                 try:
                     os.mkdir(p)
-                    print(dir_mk)
+                    print(dic_name[x], p, dir_mk)
                     continue
                 except FileNotFoundError:
                     print(dir_error)
-            print(dic_name[x], p,dir_exist)
+            print(dic_name[x], p, dir_exist)
         except:
             continue
 
@@ -240,11 +243,14 @@ def start():
         exit()
 
 
+# *****************************************************************
+
 def check_files():
     scan_student()  # 学生文件夹
     scan_answer()  # 答案文件
     scan_student_list()  # 花名册文件
     print(__borderline)
+
 
 def scan_student():
     files_count = 0
@@ -255,7 +261,7 @@ def scan_student():
                 files_count += 1
     scan.close()
     print(student_dir, "目录已找到xlsx文件数量：", files_count)
-    
+
 
 def scan_answer():
     try:
--- py12/test.py
+++ py12/test.py
@@ -4,10 +4,12 @@ from openpyxl import Workbook
 import os
 import random
 import time
+
+
 def test1():
-    wb = load_workbook(filename = "test.xlsx")
+    wb = load_workbook(filename="test.xlsx")
     # 加载后 用 wb.save("test_new.xlsx")保存
-    
+
     sheet_ranges = wb["工作表1"]
     count = 0
     # sheet_ranges['']遍历列号
@@ -16,42 +18,41 @@ def test1():
         print(x.value)
         if count == 10:
             break
-    print(count,"\n")
-    
+    print(count, "\n")
+
     # iter_rows(起始行，计算列数，计算行数)
-    for row in sheet_ranges.iter_rows(min_row = 1,max_col=3,max_row=2):
+    for row in sheet_ranges.iter_rows(min_row=1, max_col=3, max_row=2):
         for cell in row:
-            print(cell.coordinate,"格子位置")
+            print(cell.coordinate, "格子位置")
             print(cell.value)
     # print(sheet_ranges["A1"].value)
 
-    #sheet_ranges.insert_rows(7) 第七行为新增，即截断前面6行
-    #sheet_ranges.delete_cols(1,2)从第几列开始删除，删除几列
-    #sheet_ranges.delete_rows(7) 第七行为删除，后面不会补上
+    # sheet_ranges.insert_rows(7) 第七行为新增，即截断前面6行
+    # sheet_ranges.delete_cols(1,2)从第几列开始删除，删除几列
+    # sheet_ranges.delete_rows(7) 第七行为删除，后面不会补上
 
     # 完全遍历
     for row in sheet_ranges:
         break
         for value in row:
             print(value.value)
-   # wb.save("test_new.xlsx")
+    # wb.save("test_new.xlsx")
 
-    
     wb = Workbook()
     ws = wb.active
-    
+
     data = [
         ['Apples', 10000, 5000, 8000, 6000],
-        ['Pears',   2000, 3000, 4000, 5000],
+        ['Pears', 2000, 3000, 4000, 5000],
         ['Bananas', 6000, 6000, 6500, 6000],
-        ['Oranges',  500,  300,  200,  700],
+        ['Oranges', 500, 300, 200, 700],
     ]
 
     # add column headings. NB. these must be strings
     ws.append(["Fruit", "2011", "2012", "2013", "2014"])
     for row in data:
         ws.append(row)
-        
+
     tab = Table(displayName="Table1", ref="B1:E1")
 
     # Add a default style with striped rows and banded columns
@@ -72,45 +73,49 @@ Using this method ensures table name is unque through out defined names and all 
     print(files_count)
     scan.close()
 
+
 def test2():
-    wb = load_workbook(filename = "students.xlsx")
+    wb = load_workbook(filename="students.xlsx")
     sheet_ranges = wb["工作表1"]
     count = 0
     # iter_rows(起始行，计算列数，计算行数)
-    for row in sheet_ranges.iter_rows(min_row = 2,max_col=3,max_row=3):
+    for row in sheet_ranges.iter_rows(min_row=2, max_col=3, max_row=3):
         for cell in row:
             count += 1
-            print(cell.coordinate,"格子位置")
+            print(cell.coordinate, "格子位置")
             print(cell.value)
             print(count)
+
+
 def test3():
-    #生成一份答案文档
+    # 生成一份答案文档
     wb = Workbook()
     ws1 = wb.active
-    title = ["编号","答案","分值"]
+    title = ["编号", "答案", "分值"]
     datas = []
-    alpha = ["A","B","C","D","E","F"]
+    alpha = ["A", "B", "C", "D", "E", "F"]
     count = 1
     ws1.append(title)
     while count < 21:
         datas.append(count)
         datas.append(random.choice(alpha))
-        datas.append(random.randint(1,5))
+        datas.append(random.randint(1, 5))
         count += 1
         ws1.append(datas)
         print(datas)
         datas = []
-    wb.save("test3.xlsx")
+    wb.save("answer.xlsx")
+
 
 def test4():
-    #生成学生答案
-    stu = load_workbook(filename = "students.xlsx")
-    title = ["编号","答案"]
+    # 生成学生答案
+    stu = load_workbook(filename="students.xlsx")
+    title = ["编号", "答案"]
     datas = []
-    alpha = ["A","B","C","D","E","F"]
+    alpha = ["A", "B", "C", "D", "E", "F"]
     sheet_ranges = stu["工作表1"]
-    for x in sheet_ranges.iter_rows(min_row = 2,max_col=1):
-         for cell in x:
+    for x in sheet_ranges.iter_rows(min_row=2, max_col=1):
+        for cell in x:
             if cell.value is None:
                 break
             wb = Workbook()
@@ -124,13 +129,10 @@ def test4():
                 count += 1
                 ws1.append(datas)
                 datas = []
-                wb.save("student/"+str(number)+".xlsx"
-)
+                wb.save("student/" + str(number) + ".xlsx")
+
+
 if __name__ == "__main__":
     print("begin!")
-            
-    
-
-
-    
-    
+    # test3()
+    test4()
